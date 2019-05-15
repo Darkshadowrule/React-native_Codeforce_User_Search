324c1dcf0546aaec6c5b41bddad40d7556d5d305
darkshadowrule <prajjwaldxi3@gmail.com>
2019-05-15 10:53:45 +0530

    Add splash screen

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Header, Button, Spinner} from './src/components/common';
 import Search from './src/components/Search';
-
+import { SplashScreen } from 'expo';
 class App extends React.Component {
+  componentWillMount()
+  {
+    SplashScreen.hide();
+  }
   render() {
     return (
       <View>
